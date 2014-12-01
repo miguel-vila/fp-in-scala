fc1514a24e483caa4887dabc6c2da91351d68842
miguel <miguelvilag@gmail.com>
2014-12-01 18:05:57 -0500

    Ejs. 8.6 -> 8.8

--- src/main/scala/soluciones/chapter8/testing/Chapter8Solutions.scala
+++ src/main/scala/soluciones/chapter8/testing/Chapter8Solutions.scala
@@ -2,9 +2,14 @@ package soluciones.chapter8.testing
 
 import soluciones.chapter6.state.{ RNG , Simple, State }
 
-case class Gen[A](sample: State[RNG, A])
+case class Gen[A](sample: State[RNG, A]) {
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(Gen.listOfN(_,this))
+}
 
 object Gen {
+  def bet0and1: Gen[Double] = Gen( State(RNG.double) )
   def choose(start: Int, stopExclusive: Int): Gen[Int] = {
     assert(start<stopExclusive)
     val st = State(RNG.double).map { d => ( start + (stopExclusive-start)*d ).toInt }
@@ -25,4 +30,23 @@ object Gen {
   def char: Gen[Char] = Gen(choose(0,256).sample.map(_.toChar))
 
   def stringOfLength(n: Int): Gen[String] = Gen( listOfN(n, char).sample.map(_.foldLeft("")(_ + _)) )
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = {
+    for {
+      b <- boolean
+      g <- if(b) g1 else g2
+    } yield g
+  }
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val (gen1,w1) = g1
+    val (gen2,w2) = g2
+    bet0and1.flatMap { d =>
+      if(d*(w1+w2)<w1)
+        gen1
+      else
+        gen2
+    }
+  }
+
 }
