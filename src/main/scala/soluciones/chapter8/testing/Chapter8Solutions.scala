b1c6c9f5d9224063afed5f114e877878ba4e1057
miguel <miguelvilag@gmail.com>
2014-12-01 23:30:26 -0500

    Ej. 8.9

--- src/main/scala/soluciones/chapter8/testing/Chapter8Solutions.scala
+++ src/main/scala/soluciones/chapter8/testing/Chapter8Solutions.scala
@@ -1,6 +1,8 @@
 package soluciones.chapter8.testing
 
 import soluciones.chapter6.state.{ RNG , Simple, State }
+import Prop._
+import soluciones.chapter5.laziness.Stream
 
 case class Gen[A](sample: State[RNG, A]) {
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
@@ -31,12 +33,7 @@ object Gen {
 
   def stringOfLength(n: Int): Gen[String] = Gen( listOfN(n, char).sample.map(_.foldLeft("")(_ + _)) )
 
-  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = {
-    for {
-      b <- boolean
-      g <- if(b) g1 else g2
-    } yield g
-  }
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap( b => if(b) g1 else g2 )
 
   def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
     val (gen1,w1) = g1
@@ -49,4 +46,77 @@ object Gen {
     }
   }
 
+}
+
+case class Prop(run: (MaxSize,TestCases,RNG) => Result) {
+
+  def &&(other: Prop): Prop = Prop { (max,testCases,rng) =>
+    val r1 = run(max,testCases,rng)
+    if(r1.isFalsified)
+      r1
+    else
+      other.run(max,testCases,rng)
+  }
+
+  def ||(other: Prop): Prop = Prop { (max,testCases,rng) =>
+    val r1 = run(max,testCases,rng)
+    r1 match {
+      case Falsified(failure, successes) => other.run(max,testCases,rng).prependMsg(failure)
+      case r => r
+    }
+  }
+
+}
+
+object Prop {
+  type SuccessCount = Int
+  type TestCases = Int
+  type MaxSize = Int
+  type FailedCase = String
+
+  sealed trait Result {
+    def isFalsified: Boolean
+    def prependMsg(str: String): Result
+  }
+
+  case object Passed extends Result {
+    def isFalsified = false
+    def prependMsg(str: String) = this
+  }
+
+  case class Falsified(failure: FailedCase,
+                       successes: SuccessCount) extends Result {
+    def isFalsified = true
+    def prependMsg(str: String) = Falsified(s"$str\n$failure", successes)
+  }
+
+  case object Proved extends Result {
+    def isFalsified = false
+    def prependMsg(str: String) = this
+  }
+
+  /* Produce an infinite random stream from a `Gen` and a starting `RNG`. */
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch {
+        case e: Exception => Falsified(buildMsg(a, e), i)
+      }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  // String interpolation syntax. A string starting with `s"` can refer to
+  // a Scala value `v` as `$v` or `${v}` in the string.
+  // This will be expanded to `v.toString` by the Scala compiler.
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def apply(f: (TestCases,RNG) => Result): Prop =
+    Prop { (_,n,rng) => f(n,rng) }
 }
